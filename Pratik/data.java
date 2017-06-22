6d1bbe18017d4642edccbdc862de9a27d0215a6a
vimakuma <vimakuma@cisco.com>
2017-06-22 09:35:14 +0530

    Added: Data Type programs example

---
+++ Pratik/data.java
@@ -0,0 +1,15 @@
+class DataTypeDemo{
+	public static void main (String []args)
+		{
+			char a = 65;
+			byte b =10;
+			short s = 100;
+			boolean c= true;
+			System.out.println("char a="+a);
+			System.out.println("byte b="+b);
+			System.out.println("short s="+s);
+			System.out.println("boolean c="+c);
+			
+			}
+			
+}
---
+++ Pratik/pratik.java
@@ -0,0 +1,14 @@
+class HelloWord{
+ public static void main (String[]args)
+	{
+	System.out.println("pratik more");
+	}
+
+
+}
+class HelloWord1{
+	public static void main (String[]args)
+	{
+	System.out.println("vimal sir");	
+	}
+}
